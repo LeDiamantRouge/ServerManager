4aa4ed62d0bc6efaa160d7a85d4bc77c50651dd5
LeDiamantRouge <98910451+LeDiamantRouge@users.noreply.github.com>
2023-10-24 14:30:01 +0200

    Add multi lobby support

--- src/main/java/fr/lediamantrouge/servermanager/bungeelisteners/ConnectBungeeCordEvent.java
+++ src/main/java/fr/lediamantrouge/servermanager/bungeelisteners/ConnectBungeeCordEvent.java
@@ -4,23 +4,34 @@ import fr.lediamantrouge.servermanager.BungeeMain;
 import fr.lediamantrouge.servermanager.CommonMain;
 import fr.lediamantrouge.servermanager.servermanager.Server;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class ConnectBungeeCordEvent implements Listener {
 
     @EventHandler
     public void onServerConnect(ServerConnectEvent e) {
         if (e.getTarget().getName().equals("fallback")) {
+            List<ServerInfo> lobbys = new ArrayList<>();
             for (Server server : CommonMain.getInstance().getServerManager().getServers()) {
                 if (server.getTemplate().equalsIgnoreCase("Lobby") && BungeeMain.getInstance().getProxy().getServerInfo(server.getName()) != null) {
-                    e.setTarget(BungeeMain.getInstance().getProxy().getServerInfo(server.getName()));
-                    return;
+                    lobbys.add(BungeeMain.getInstance().getProxy().getServerInfo(server.getName()));
                 }
             }
-            e.getPlayer().disconnect(new TextComponent("§cAucun serveur trouvé pour vous connecter..."));
+            if (lobbys.isEmpty()) {
+                e.getPlayer().disconnect(new TextComponent("§cAucun serveur trouvé pour vous connecter..."));
+                return;
+            }
+            int random = new Random().nextInt(lobbys.size());
+            e.setTarget(lobbys.get(random));
+            e.getPlayer().sendMessage(new TextComponent("§7Vous avez été connecté à " + lobbys.get(random)));
         }
     }
 
